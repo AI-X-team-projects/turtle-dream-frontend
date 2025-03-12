ee88f9aec16df4877d422d78be0b957bb0a9941c
Kimbang1 <arto135@naver.com>
2025-03-12 15:28:56 +0900

    차트작업 색상 및 폰트 개선

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import Login from "./pages/login/Login";
 import Header from './common/Header';
 import theme from './theme/theme';
+import ChartP from './pages/chart/ChartP';
 
 const Root = styled.div`
   width: 100%;
@@ -38,6 +39,7 @@ function App() {
           <Layout>
             <Routes>
               <Route path="/" element={<Login />} />
+              <Route path="/ChartP" element={<ChartP />} />
             </Routes>
           </Layout>
         </Router>
